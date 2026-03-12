af8fa36bdb919152dc56d8f433463969c2207c77
micheleespacodosaber-cmyk <micheleespacodosaber@gmail.com>
2026-03-12 16:31:47 -0300

    Fé

--- produto/forms.py
+++ produto/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 
 class ProdutoForm(forms.Form):
-    codigo = forms.IntegerField(label='Código')
-    nome = forms.CharField(max_length=70, label='Nome')
-    preco_compra = forms.FloatField(label='Preço de compra')
-    preco_venda = forms.FloatField(label='Preço de venda')
-    cor = forms.CharField(max_length=20, label='Cor')
-    data_fabricacao = forms.DateField(label='Data de fabricação',input_formats=['%d/%m/%Y'],widget=forms.DateInput(attrs={'placeholder': 'dd/mm/aaaa'}))
-    imagem = forms.CharField(max_length=25, label='Imagem')
+    codigo = forms.IntegerField(required=True)
+    nome = forms.CharField(max_length=70, required=True)
+    preco_compra = forms.FloatField(required=True)
+    preco_venda = forms.FloatField(required=True)
+    cor = forms.CharField(max_length=20, required=True)
+    data_fabricacao = forms.DateField(
+        input_formats=['%d/%m/%Y'],
+        required=True
+    )
+    imagem = forms.CharField(max_length=25, required=True)
